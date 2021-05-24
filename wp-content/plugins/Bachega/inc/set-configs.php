e3914fff89cd773515aa4f57a70a71da1ef1518a
devna <naelson.g.saraiva@gmail.com>
2021-05-24 11:31:00 -0400

    up

--- wp-content/plugins/Bachega/inc/set-configs.php
+++ wp-content/plugins/Bachega/inc/set-configs.php
@@ -5,9 +5,9 @@ function auto_config()
     $autoC_post = array('post_title' => 'spreadsheet_req', 'post_content'  => '', 'post_status'   => 'publish', 'post_author'   => 1, 'post_category' => array(1), 'post_type'     => 'page');
     $flag = wp_insert_post($autoC_post);
     if ($flag > 0) {
-        echo '<div class="alert m-2 alert-success" role="alert">Successfull PageID=' . $flag . '</div>';
+        echo '<div class="alert m-2 alert-success" role="alert">Successfull pageID=' . $flag . '</div>';
     } else {
-        echo '<div class="alert m-2 alert-danger" role="alert">Error PageID=' . $flag . '</div>';
+        echo '<div class="alert m-2 alert-danger" role="alert">Error pageID=' . $flag . '</div>';
     }
 }
 
