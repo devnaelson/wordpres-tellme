afaca57cc7eda07451c951701002ff939f999a6b
devna <naelson.g.saraiva@gmail.com>
2021-06-14 12:45:39 -0400

    Up

--- wp-content/plugins/Bachega/pages/upload.php
+++ wp-content/plugins/Bachega/pages/upload.php
@@ -1,18 +1,17 @@
-<div class="row">
+<div class="row bgbox text-center p-5 w-100">
 
-    <div class="col-12 bg-danger">
+    <div class="col-auto bg-danger w-100">
         s
     </div>
-    <div class="col-12 bg-warning">
+    <div class="col-auto bg-warning w-100">
         sd
     </div>
-    <div class="col-12">
+    <div class="col-auto w-100">
         <?php
         $resultFiels = get_option('myfields');
         for ($bI = 0; $bI < count($resultFiels); $bI++) {
-            echo '<div draggable="true" class="box-fields">'.$resultFiels[$bI]['label'].'</div>';
+            echo '<div draggable="true" class="box-fields">' . $resultFiels[$bI]['label'] . '</div>';
         }
         ?>
     </div>
-
 </div>
