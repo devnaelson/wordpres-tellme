6e36fcc291fbad3be2033ba616fa9838ab5861d5
devna <naelson.g.saraiva@gmail.com>
2021-06-26 10:17:19 -0400

    Up

--- wp-content/plugins/Bachega/pages/upload.php
+++ wp-content/plugins/Bachega/pages/upload.php
@@ -21,7 +21,7 @@
         $dPFields = array();
         $dPFields = $wpdb->get_results("SHOW COLUMNS FROM bd_erp_peoples");
         $resultFiels = get_option('myfields');
-        for ($bI = 0; $bI < count($resultFiels); $bI++) echo '<div draggable="true" class="box-fields lessthan" data-side="left" data-column="' . $resultFiels[$bI]['name'] . '" data-table="bd_meta_peoples" >' . $resultFiels[$bI]['name'] . '</div>';
+        for ($bI = 0; $bI < count($resultFiels); $bI++) echo '<div draggable="true" class="box-fields lessthan" data-side="left" data-column="' . $resultFiels[$bI]['name'] . '" data-table="bd_erp_peoplemeta" >' . $resultFiels[$bI]['name'] . '</div>';
         for ($bI = 0; $bI < count($dPFields); $bI++) echo '<div draggable="true" class="box-fields" data-people-field="' . $dPFields[$bI]->Field . '" data-table="bd_erp_peoples">' . $dPFields[$bI]->Field . '</div>';
         ?>
     </div>
