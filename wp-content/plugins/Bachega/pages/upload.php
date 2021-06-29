c4c2df959ee42cd21fbc13150df23ce5c896c664
devna <naelson.g.saraiva@gmail.com>
2021-06-29 07:54:35 -0400

    Up

--- wp-content/plugins/Bachega/pages/upload.php
+++ wp-content/plugins/Bachega/pages/upload.php
@@ -15,14 +15,16 @@
     </div>
     <div class="col-4 bgbox" id="sheet_detect">
     </div>
-    <div class="col-4 bgbox">
-        <?php
-        global $wpdb;
-        $dPFields = array();
-        $dPFields = $wpdb->get_results("SHOW COLUMNS FROM bd_erp_peoples");
-        $resultFiels = get_option('myfields');
-        for ($bI = 0; $bI < count($resultFiels); $bI++) echo '<div draggable="true" class="box-fields lessthan" data-side="left" data-column="' . $resultFiels[$bI]['name'] . '" data-table="bd_erp_peoplemeta" >' . $resultFiels[$bI]['name'] . '</div>';
-        for ($bI = 0; $bI < count($dPFields); $bI++) echo '<div draggable="true" class="box-fields" data-people-field="' . $dPFields[$bI]->Field . '" data-table="bd_erp_peoples">' . $dPFields[$bI]->Field . '</div>';
-        ?>
+      <div class="col-4 bgbox">
+      <div class="scrollExec">
+          <?php
+          global $wpdb;
+          $dPFields = array();
+          $dPFields = $wpdb->get_results("SHOW COLUMNS FROM bd_erp_peoples");
+          $resultFiels = get_option('myfields');
+          for ($bI = 0; $bI < count($resultFiels); $bI++) echo '<div draggable="true" class="box-fields lessthan" data-side="left" data-column="' . $resultFiels[$bI]['name'] . '" data-table="bd_erp_peoplemeta" >' . $resultFiels[$bI]['name'] . '</div>';
+          for ($bI = 0; $bI < count($dPFields); $bI++) echo '<div draggable="true" class="box-fields" data-people-field="' . $dPFields[$bI]->Field . '" data-table="bd_erp_peoples">' . $dPFields[$bI]->Field . '</div>';
+          ?>
+      </div>
     </div>
 </div>
