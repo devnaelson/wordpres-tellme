975683111ac510701b5cb541d89c4dd9808427eb
devna <naelson.g.saraiva@gmail.com>
2021-07-01 14:48:35 -0400

    Up

--- wp-content/plugins/test/test.php
+++ wp-content/plugins/test/test.php
@@ -20,16 +20,51 @@ add_action('admin_menu', 'menu_unsub_add_pages');
 function menu_unsub_add_pages()
 {
     add_menu_page(
-        __('Planilhas', 'textdomain'),
-        __('Planilhas', 'textdomain'),
+        __('TEST', 'textdomain'),
+        __('TEST', 'textdomain'),
         'manage_options',
         'exc-main',
-        'menu_unsub_page_callback',
+        'menu_unsub_page_callbackTEST',
         '',
         60
     );
 }
-function menu_unsub_page_callback()
+function menu_unsub_page_callbackTEST()
 {
     echo "asasa";
+}
+
+add_action( 'wp_ajax_my_action', 'my_action' );
+
+function my_action() {
+	global $wpdb; // this is how you get access to the database
+
+	$whatever = intval( $_POST['whatever'] );
+
+	$whatever += 10;
+
+        echo $whatever;
+
+	wp_die(); // this is required to terminate immediately and return a proper response
+}
+
+add_action( 'admin_footer', 'my_action_javascript' ); // Write our JS below here
+
+function my_action_javascript() { ?>
+	<script type="text/javascript" >
+
+	jQuery(document).ready(function($) {
+
+		var data = {
+			'action': 'my_action',
+			'whatever': 1234
+		};
+
+		// since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+		jQuery.post(ajaxurl, data, function(response) {
+			alert('Got this from the server: ' + response);
+		});
+	});
+    console.log(ajaxurl)
+	</script> <?php
 }
