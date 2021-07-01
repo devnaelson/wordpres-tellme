80dfaa5cd823ac7bb11fa411a9bb3b43f69914c4
devna <naelson.g.saraiva@gmail.com>
2021-07-01 15:34:18 -0400

    up

--- wp-content/plugins/test/global.js
+++ wp-content/plugins/test/global.js
@@ -1,1 +1,12 @@
-alert("asas")
+
+const xhrSend = new XMLHttpRequest();
+let formD = new FormData();
+formD.append('action', 'my_action');
+formD.append('whatever', '1234');
+xhrSend.open("POST", ajaxurl);
+xhrSend.send(formD);
+xhrSend.onreadystatechange = function() {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        alert(this.responseText)
+    }
+ }
--- wp-content/plugins/test/test.php
+++ wp-content/plugins/test/test.php
@@ -35,9 +35,23 @@ function menu_unsub_page_callbackTEST()
 }
 
 
-add_action( 'wp_ajax_my_action', 'calledFilePHP' );
-function calledFilePHP( $page_template ) {
-    $page_template = dirname( __FILE__ ) . '/ajax.php';
+// add_action( 'wp_ajax_my_action', 'calledFilePHP' );
+// function calledFilePHP( $page_template ) {
+//     $page_template = dirname( __FILE__ ) . '/ajax.php';
+// }
+
+add_action( 'wp_ajax_my_action', 'my_action' );
+
+function my_action() {
+	global $wpdb; // this is how you get access to the database
+
+	$whatever = intval( $_POST['whatever'] );
+
+	$whatever += 10;
+
+        echo $whatever;
+
+	wp_die(); // this is required to terminate immediately and return a proper response
 }
 
 add_action( 'admin_enqueue_scripts', 'calledJS' );
